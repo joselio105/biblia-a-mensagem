077b3ee0343282d7185485eede30099328aa6d86
José Hélio Veríssimoo Jr <joselio105@users.noreply.github.com>
2024-10-23 16:30:57 -0300

    build: Start react native app

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header";
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
